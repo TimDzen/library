290acdb01e1bd9df7fc85f3111b02fb4b79ad20f
TimDzen <140600844+TimDzen@users.noreply.github.com>
2023-10-26 17:07:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 from datetime import datetime
 
-from flask import Flask
+from flask import Flask, request, redirect, url_for
 from flask import render_template
-
 
 from database import db, Book, Genre
 
@@ -10,33 +9,31 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
 db.init_app(app)
 
-
 with app.app_context():
     db.drop_all()
     db.create_all()
-
 
     genre1 = Genre(name='Роман')
     genre2 = Genre(name='Ужасы')
     db.session.add_all([genre1, genre2])
     db.session.commit()
 
-
     books = []
     for _ in range(30):
         book1 = Book(title='Книга романа {}'.format(_),
                      author='Автор романа {}'.format(_),
                      genre_id=genre1.id,
-                     created_at=datetime.utcnow())
+                     created_at=datetime.utcnow()
+                     )
         book2 = Book(title='Книга ужасов {}'.format(_),
                      author='Автор книги {}'.format(_),
                      genre_id=genre2.id,
-                     created_at=datetime.utcnow())
+                     created_at=datetime.utcnow()
+                     )
         books.extend([book1, book2])
 
     db.session.add_all(books)
     db.session.commit()
-
 
 
 @app.route('/')
@@ -49,7 +46,20 @@ def index():
 def genre(genre_id):
     genre = Genre.query.get_or_404(genre_id)
     books = Book.query.filter_by(genre_id=genre_id).all()
-    return render_template('genre.html', genre=genre, books=books)
+    return render_template('genre.html',
+                           genre=genre,
+                           books=books
+                           )
+
+
+@app.route("/update_status", methods=['POST'])
+def update_status():
+    book_id = request.form.get('book_id')
+    is_read = request.form.get('is_read')
+    book = Book.query.get_or_404(book_id)
+    book.is_read = is_read
+    db.session.commit()
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
